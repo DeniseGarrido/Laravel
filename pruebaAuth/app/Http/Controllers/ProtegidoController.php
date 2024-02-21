03aac0a91a71300bfea497216b2b18df9266c37a
DeniseDAW <garrido.tamarit.denise@gmail.com>
2024-02-21 11:25:18 +0100

    pruebaAuth final pt7

---
+++ pruebaAuth/app/Http/Controllers/ProtegidoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProtegidoController extends Controller
+{
+    protected function protegido()
+    {
+        return "Bienvenido/a a la zona protegida.";
+    }
+}
